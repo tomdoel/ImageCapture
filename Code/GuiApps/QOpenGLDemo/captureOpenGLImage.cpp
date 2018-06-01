1b5d7a2ecb4f23123a369f373dcc182f5a303780
Tom Doel <tomdoel@users.noreply.github.com>
2018-06-01 16:30:06 +0100

    Made  OpenGL capture fill whole screen

--- Code/GuiApps/QOpenGLDemo/captureOpenGLImage.cpp
+++ Code/GuiApps/QOpenGLDemo/captureOpenGLImage.cpp
@@ -125,10 +125,10 @@ void OpenGLImage::initializeGL()
 
     GLfloat vertices[] = {
     //  2xposition, 3xcolour, 2xtec coords
-        -0.5f,  0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // Top left
-         0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // Top right
-         0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, // Bottom right
-        -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f  // Bottom left
+        -1.0f,  1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // Top left
+         1.0f,  1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // Top right
+         1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, // Bottom right
+        -1.0f, -1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f  // Bottom left
     };
 
     glBindBuffer(GL_ARRAY_BUFFER, m_vertex_buffer_id);
